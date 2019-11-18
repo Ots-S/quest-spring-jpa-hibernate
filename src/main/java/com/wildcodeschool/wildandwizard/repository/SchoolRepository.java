97375afa3ba785fabe8222bff85bc5aeef5b6c4b
Sebastien Ots <ots.sebastien@gmail.com>
2019-11-18 20:53:23 +0100

    correction

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,7 +1,5 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
-
 import com.wildcodeschool.wildandwizard.entity.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
